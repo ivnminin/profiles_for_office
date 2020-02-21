73c00158b0f2f5bb8c269a105ae5f313b94fa9d9
ivn <ivn@li.ru>
2020-02-21 15:35:32 +0300

    debug=True

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -6,11 +6,13 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_dropzone import Dropzone
 from flask_wtf.csrf import CsrfProtect
+from werkzeug.debug import DebuggedApplication
 
 import config
 from . import custom_filters
 
 app = Flask(__name__)
+app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
 
 app.config.from_object(os.environ.get('FLASK_ENV') or 'config.DevelopementConfig')
 custom_filters.generate_custom_filter(app)
